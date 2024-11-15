88fa41aa4b34af7b3b3dc8aaecaf8c0ff73c3337
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2024-11-15 17:16:41 -0500

    WASD
    added WASD movement, for people who prefer it

--- player.gd
+++ player.gd
@@ -8,13 +8,13 @@ func _ready():
 func _physics_process(delta):
 	# movement, also I know there's better ways but this works better if we want to customize stuff later
 	velocity = Vector2(0,0)
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("w"):
 		velocity.y -= SPEED
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("s"):
 		velocity.y += SPEED
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("a"):
 		velocity.x -= SPEED
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("d"):
 		velocity.x += SPEED
 	
 	move_and_slide()
